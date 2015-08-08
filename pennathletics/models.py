b2990e3575c196f9cea55152faf1789d771f974e
Adel Qalieh <aqalieh95@gmail.com>
2015-08-08 03:59:32 -0400

    Add model for athletes

---
+++ pennathletics/models.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+
+class Athlete(object):
+    """Class for an athlete on roster"""
+
+    def __init__(self, no, name, pos, ht, wt, yr, hometown):
+        self.no = no
+        self.name = name
+        self.pos = pos
+        self.wt = wt
+        self.yr = yr
+        self.hometown = hometown
+
+    def __repr__(self):
+        print("<Athlete #{} {}".format(self.no, self.name))
