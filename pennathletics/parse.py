f07a901ff88f3c23fed8b5e0c22ffc93923b1ffd
Adam Domingoes <adamdom@pennlabs.org>
2015-11-07 16:14:28 -0500

    Added parsing function for date from schedule

--- pennathletics/parse.py
+++ pennathletics/parse.py
@@ -1,6 +1,7 @@
 """Utilities for parsing pages"""
 
 from collections import namedtuple
+from datetime import datetime
 
 Home = namedtuple('Home', ['town', 'school'])
 
@@ -24,3 +25,18 @@ def parse_hometown(hometown_str):
     """
     town, school = hometown_str.split("(")
     return Home(town[:-1], school[:-1])
+
+def parse_date_in_schedule(dayAndMonth, time, year):
+    """Will take in the values from the day of the Week, month, day, and year and 
+    return a python datetime object
+
+    >>> parse_date_in_schedule("Tue, Nov 20", "8:00 PM", 2014)
+    2014-11-20 20:00:00
+    """
+    
+    date_object = datetime.strptime('{}{}{}{}{}{}'.format(dayAndMonth[5:], " ", str(year), " ", time[0:4], time[5:7]), '%b %d %Y %I:%M%p')
+    print date_object
+    return date_object
+
+
+parse_date_in_schedule("Tue, Nov 20", "8:00 PM", 2014)
