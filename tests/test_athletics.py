c197fc30d211cf53bca884d597845557376de99d
Adam Domingoes <adamdom@wharton.upenn.edu>
2016-02-13 14:00:37 -0500

    More tests

--- tests/test_athletics.py
+++ tests/test_athletics.py
@@ -1,6 +1,6 @@
 from nose.tools import ok_, eq_
 from pennathletics.athletes import get_roster, get_player
-
+from pennathletics.scraper import get_schedule
 
 class TestAthletics():
     def test_roster(self):
@@ -11,3 +11,13 @@ class TestAthletics():
 
     def test_player_number(self):
         eq_(get_player("M_Basketball", 2013, no=1)[0].height, '6-2')
+
+    def test_player_hometown(self):
+        eq_(get_player("M_Basketball",
+                2002, Hometown="Germantown,Md. (DeMatha)")[0].weight, 215)
+
+    def test_player_softball(self):
+        ok_(get_roster("W_Softball", 2002) == [])
+
+    def test_schedule(self):
+        ok_(get_schedule("M_Soccer", 2011) != [])
