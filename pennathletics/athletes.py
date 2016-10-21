f96950a4cb87b00ca27fbf8c5bd3bd958d65f1a7
Adel Qalieh <adelq@seas.upenn.edu>
2016-10-21 11:31:24 -0400

    Add JSON parser for getting roster

--- pennathletics/athletes.py
+++ pennathletics/athletes.py
@@ -1,17 +1,22 @@
-from .models import Athlete
-from . import scraper as s
+import requests
+from munch import munchify
+from . import BASE_URL
+
+ROSTER_URL = BASE_URL + "/groups?include=members&sport_codes={sport}&seasons={year}"
 
 
 def get_roster(sport, year):
-    raw = s.scrape_roster(sport, year)
-    return [Athlete(player) for player in raw]
+    """Get roster, takes in sport code and year in YYYY format"""
+    uri = ROSTER_URL.format(sport=sport, year=year)
+    data = requests.get(uri).json()[0]["players"]
+    return munchify(data)
 
 
 def get_player(sport, year, **kwargs):
     """Returns players with given attributes. *Sport and Year are required
 
-    >>> int(get_player('M_Basketball', 2015, no = 0)[0].weight)
-    225
+    >>> int(get_player('m-baskbl', 2015, jersey=0)[0].weight)
+    225 lbs
     """
     potential_list = get_roster(sport, year)
     list_to_return = []
--- tests/test_athletics.py
+++ tests/test_athletics.py
@@ -4,10 +4,10 @@ from pennathletics.athletes import get_roster, get_player
 
 class TestAthletics():
     def test_roster(self):
-        ok_(get_roster("M_Basketball", 2015) != [])
+        ok_(get_roster("m-baskbl", 2015) != [])
 
     def test_player_empty(self):
-        ok_(get_player("M_Basketball", 2014) != [])
+        ok_(get_player("m-baskbl", 2014) != [])
 
     def test_player_number(self):
-        eq_(get_player("M_Basketball", 2013, no=1)[0].height, '6-2')
+        eq_(get_player("m-baskbl", 2013, jersey=1)[0].height, "6'2\"")
