459546a9cedb8e9cf3bee67edb4a76d37874f03b
Adel Qalieh <aqalieh95@gmail.com>
2016-11-05 00:38:59 -0400

    Add a few more tests for variety

--- tests/test_athletics.py
+++ tests/test_athletics.py
@@ -11,3 +11,11 @@ class TestAthletics():
 
     def test_player_number(self):
         eq_(get_player("m-baskbl", 2013, jersey=1)[0].height, "6'2\"")
+
+    def test_player_hometown(self):
+        player = get_player("m-baskbl", 2012, homeTown="Belfast, Ireland")[0]
+        eq_(player.weight, '210 lbs')
+
+    def test_player_softball(self):
+        # 19 players on the 2013 softball team
+        eq_(len(get_roster("w-softbl", 2013)), 19)
